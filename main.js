3d9cdd786f21797666ea8f41a08b3f81e67f71a4
tchi-sama <tchisama41@gmail.com>
2023-05-12 21:40:19 +0100

    new stuff added to the state

--- main.js
+++ main.js
@@ -11,37 +11,25 @@ const loop =(times,fun)=>{for (let i = 0; i < times; i++) {fun(i)}};
 const print =(v)=> console.log(v)
 const hide = (ele)=> ele.style.display="none"
 const show = (ele)=> ele.style.display="block";
-const text = (ele,text)=> ele.innerHTML=text;
 
 
-let button = getAll("button")
 
 // onClick(button,()=>console.log("hello world"))
 // onHover(button,()=>console.log("over"))
 // onOut(button,()=>console.log("out"))
 
-i=false;
-getAll("button").forEach(btn=>{
-    onClick(btn,()=>{
-        i=!i
-        is(i,()=>{
-            // hide(get("#button"))
-            text(get("#button"),"hi")
-        },()=>{
-            // show(get("#button"))
-            text(get("#button"),"hello")
-        })
-    })
-})
-
-console.log(button)
-
 
 //console.log(and(true,true,true)); //true
 //console.log(or(false,false,false)); // false
 //console.log(not(false)); // true
-
 //console.log(is(true,"hello",()=>{console.log("hi")})); // hello
+
+
+
+
+
+
+
 
 
 
