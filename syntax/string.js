2df35995bc7e71bc34b1788e0218a641bac59a8c
Z-Najdaoui <zobirnajdaoui@gmail.com>
2023-05-14 14:24:56 +0100

    short capitalize

--- syntax/string.js
+++ syntax/string.js
@@ -1,20 +1,8 @@
-const String = {
-  /**
-   * The capitalize() method capitalizes the first letter of every word in a string.
-   * @param {string} str
-   * @returns {string}
-   * @example
-   * capitalize("hello world") // output:"Hello World"
-   */
-  capitalize: (str) => {
-    return str
-      .split(" ")
-      .map((word) => {
-        return /^[A-Za-z]+$/.test(word)
-          ? word[0].toUpperCase() + word.slice(1)
-          : word;
-      })
-      .join(" ");
-  },
-};
-export default String;
+/**
+ * The capitalize() method capitalizes the first letter of every word in a string.
+ * @param {string} str
+ * @returns {string}
+ * @example
+ * capitalize("hello world") // output:"Hello World"
+ */
+export function capitalize(str) {return str.split(" ").map((word) => {return word[0].toUpperCase() + word.slice(1)}).join(" ");}
