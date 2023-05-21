704581b4705764451e01dc301d9088273ed5883b
whybe7 <whybe7@hotmail.com>
2023-05-21 17:38:53 +0100

    Test env for both js browser and NodeJs

--- test/index.js
+++ test/index.js
@@ -1,2 +1,11 @@
 // start importing all your test files here
 // NOTE: this file is linked to the index.html file
+
+// 1. install handy via : npm i @handy.js/handy
+// 2. or link the package locally by going to the package directory and running : npm link
+// then run : npm link @handy.js/handy
+
+import "@handy.js/handy";
+import { HOperators } from "@handy.js/handy";
+
+console.log(); // false
