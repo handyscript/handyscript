b2218f6539afdba24c1d3685b90b4f2a5c59de32
whybe7 <whybe7@hotmail.com>
2023-05-21 15:09:40 +0100

    bundle the pkg into CJS & ESM format for both NodeJS/Browser support

--- packages/handy/rollup.config.js
+++ packages/handy/rollup.config.js
@@ -4,11 +4,20 @@ import babel from 'rollup-plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
-  input: 'index.js',
-  output: {
-    file: 'dist/handy.min.cjs',
-    format: 'cjs',
-  },
+  input: 'index.ts',
+  output: [
+    {
+      file: 'dist/handy.min.cjs',
+      format: 'cjs', // using CJS format for Node.js.
+      sourcemap: true,
+    },
+    {
+      file: 'dist/handy.min.js',
+      format: 'esm', // using ESM format for Browsers and sweet able for script tag
+      sourcemap: true,
+    }
+  ],
+
   plugins: [
     resolve(),
     commonjs(),
