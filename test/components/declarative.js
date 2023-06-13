d3fc20044de75ade1c18803886d902ee7f6b9c3f
vvhybe <vvhybe@hotmail.com>
2023-06-13 10:38:48 +0100

    Setup every pkg with it's own git history

--- test/components/declarative.js
+++
@@ -1,8 +0,0 @@
-import state from "../hooks/state.js";
-import render from "../syntax/databinding.js";
-
-const root = document.querySelector("#root");
-
-function Counter(){
-    const [count, setCount, subscribeListner] = state(0);
-}
