c932c7a4e164edac8c8fc88c8a840989a18d37d1
vvhybe <vvhybe@hotmail.com>
2023-06-17 15:30:23 +0100

    json index import

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import "./lib/array";
 import "./lib/string";
 import "./lib/math";
 import "./lib/number";
+import "./lib/json";
 import HOperators from "./lib/operators";
 import HashMap from "./lib/hashmap";
 import Matrix from "./lib/matrix";
