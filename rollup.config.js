6fce368cb403b5b642c414b732112eac9fc87aa6
vvhybe <vvhybe@hotmail.com>
2023-09-26 13:24:01 +0100

    v1.3.0

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import typescript from "rollup-plugin-typescript2";
+import json from "@rollup/plugin-json";
 
 export default {
   input: "index.ts",
@@ -21,6 +22,7 @@ export default {
   plugins: [
     resolve(),
     commonjs(),
+    json(),
     babel({
       exclude: "node_modules/**",
     }),
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,39 +3,22 @@ import "../lib/date";
 
 import Utils from "handyutility";
 
-type AddressSchema = {
-  city: string;
-  street: string;
-};
-
 type UserSchema = {
   name: string;
   age?: number;
   email: string;
-  address?: AddressSchema;
 };
 
 const data: UserSchema = {
   name: "John Doe",
   age: 30,
   email: "johndoe@example.com",
-  address: {
-    city: "New York",
-    street: "123 Main St"
-  }
 };
 
 const schema: Schema<UserSchema> = {
   name: { type: String, required: true },
   age: { type: Number },
   email: { type: String, required: true },
-  address: {
-    type: Object,
-    properties: {
-      city: { type: String },
-      street: { type: String }
-    }
-  }
 };
 
 const isValid = JSON.validateSchema(data, schema);
