1b1332a84e4f157b66a663eeb5730c0bc52b4208
vvhybe <vvhybe@hotmail.com>
2023-11-13 01:20:52 +0100

    extend the date module with more methods

--- lib/date.ts
+++ lib/date.ts
@@ -11,13 +11,39 @@ declare global {
 
   interface Date {
     /**
+     * Returns true if the year is a leap year
+     */
+    isLeapYear(): boolean;
+
+    /**
      * Returns the number of days in the month
      */
     daysInMonth(): number;
+    
     /**
      * Returns the number of days in the year
      */
     daysInYear(): number;
+
+    /**
+     * Return how many years ago the date was till today
+     */
+    yearsAgo(): number;
+
+    /**
+     * Return how many months ago the date was till today
+     */
+    monthsAgo(): number;
+
+    /**
+     * Return how many weeks ago the date was till today
+     */
+    weeksAgo(): number;
+
+    /**
+     * Return how many days ago the date was till today
+     */
+    daysAgo(): number;
   }
 }
 
@@ -26,12 +52,39 @@ Date.timestamp = async function (timeZone=Intl.DateTimeFormat().resolvedOptions(
 	return new Date(data.fulldate);
 };
 
+Date.prototype.isLeapYear = function () {
+	const year = this.getFullYear();
+	return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+};
+
 Date.prototype.daysInMonth = function () {
+	// check if it's February and the year is leap
+	if (this.getMonth() === 1 && this.isLeapYear()) return 29;
 	return new Date(this.getFullYear(), this.getMonth() + 1, 0).getDate();
 };
 
 Date.prototype.daysInYear = function () {
-	return new Date(this.getFullYear(), 12, 0).getDate();
+	return this.isLeapYear() ? 366 : 365;
+};
+
+Date.prototype.yearsAgo = function () {
+	const today = new Date();
+	return today.getFullYear() - this.getFullYear();
+};
+
+Date.prototype.monthsAgo = function () {
+	const today = new Date();
+	return (today.getFullYear() - this.getFullYear()) * 12 + today.getMonth() - this.getMonth();
+};
+
+Date.prototype.weeksAgo = function () {
+	const today = new Date();
+	return Math.floor((today.getTime() - this.getTime()) / (1000 * 60 * 60 * 24 * 7));
+};
+
+Date.prototype.daysAgo = function () {
+	const today = new Date();
+	return Math.floor((today.getTime() - this.getTime()) / (1000 * 60 * 60 * 24));
 };
 
 export default Date;
