9bc6eb3d66ef7a3fc17fab7b417a99593b2f4c1e
vvhybe <vvhybe@hotmail.com>
2023-11-20 21:31:07 +0100

    format file

--- lib/number.ts
+++ lib/number.ts
@@ -1,5 +1,5 @@
 /// ------------------------------- HANDY NUMBER © HandyScript 6m/2d/23y -------------------------------
-import Math from './math';
+import Math from "./math";
 
 declare global {
 	interface Number {
@@ -50,26 +50,26 @@ declare global {
 Number.prototype.toHuman = function (): string {
 	const num = this.valueOf();
 	const si = [
-		{ value: 1, symbol: '' },
-		{ value: 1e3, symbol: 'K' },
-		{ value: 1e6, symbol: 'M' },
-		{ value: 1e9, symbol: 'B' },
-		{ value: 1e12, symbol: 'T' },
-		{ value: 1e15, symbol: 'P' },
-		{ value: 1e18, symbol: 'E' }
+		{ value: 1, symbol: "" },
+		{ value: 1e3, symbol: "K" },
+		{ value: 1e6, symbol: "M" },
+		{ value: 1e9, symbol: "B" },
+		{ value: 1e12, symbol: "T" },
+		{ value: 1e15, symbol: "P" },
+		{ value: 1e18, symbol: "E" }
 	];
 	const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
 	let i;
 	for (i = si.length - 1; i > 0; i--) if (num >= si[i].value) break;
-	return (num / si[i].value).toFixed(2).replace(rx, '$1') + si[i].symbol;
+	return (num / si[i].value).toFixed(2).replace(rx, "$1") + si[i].symbol;
 };
 
-Number.prototype.toReadable = function (separator = '-'): string {
+Number.prototype.toReadable = function (separator = "-"): string {
 	const num = this.valueOf();
 	const rx = /(\d+)(\d{3})/;
 	return String(num).replace(/^\d+/, function (w) {
 		while (rx.test(w)) {
-			w = w.replace(rx, '$1' + separator + '$2');
+			w = w.replace(rx, "$1" + separator + "$2");
 		}
 		return w;
 	});
@@ -77,7 +77,7 @@ Number.prototype.toReadable = function (separator = '-'): string {
 
 Number.prototype.isFinite = function (): boolean {
 	return !(
-		typeof this !== 'number' ||
+		typeof this !== "number" ||
 		this !== this ||
 		this === Infinity ||
 		this === -Infinity
@@ -85,115 +85,108 @@ Number.prototype.isFinite = function (): boolean {
 };
 
 Number.prototype.isSafeNumber = function (): boolean {
-	return typeof this === 'number' && Math.abs(this) <= Number.MAX_SAFE_INTEGER;
+	return typeof this === "number" && Math.abs(this) <= Number.MAX_SAFE_INTEGER;
 };
 Number.prototype.toWords = function (): string {
 	const number = this.valueOf();
 	const LESS_THAN_TWENTY = [
-		'zero',
-		'one',
-		'two',
-		'three',
-		'four',
-		'five',
-		'six',
-		'seven',
-		'eight',
-		'nine',
-		'ten',
-		'eleven',
-		'twelve',
-		'thirteen',
-		'fourteen',
-		'fifteen',
-		'sixteen',
-		'seventeen',
-		'eighteen',
-		'nineteen'
+		"zero",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+		"ten",
+		"eleven",
+		"twelve",
+		"thirteen",
+		"fourteen",
+		"fifteen",
+		"sixteen",
+		"seventeen",
+		"eighteen",
+		"nineteen",
 	];
 
 	const TENTHS_LESS_THAN_HUNDRED = [
-		'zero',
-		'ten',
-		'twenty',
-		'thirty',
-		'forty',
-		'fifty',
-		'sixty',
-		'seventy',
-		'eighty',
-		'ninety'
+		"zero",
+		"ten",
+		"twenty",
+		"thirty",
+		"forty",
+		"fifty",
+		"sixty",
+		"seventy",
+		"eighty",
+		"ninety",
 	];
-
-	var words;
 
 	if (!number.isFinite()) {
 		throw new TypeError(
-			'Not a finite number: ' + number + ' (' + typeof number + ')'
+			"Not a finite number: " + number + " (" + typeof number + ")"
 		);
 	}
 	if (!number.isSafeNumber()) {
 		throw new RangeError(
-			'Input is not a safe number, it’s either too large or too small.'
+			"Input is not a safe number, it’s either too large or too small."
 		);
 	}
 
-	let generateWords = function (
-		n_umber: number,
-		w_ords?: Array<string | undefined>
-	) {
-		var remainder, word;
+	const generateWords = function (number: number, words?: Array<string | undefined>): string {
+		let remainder, word;
 		// We’re done
-		if (n_umber === 0) {
-			return !w_ords ? 'zero' : w_ords.join(' ').replace(/,$/, '');
+		if (number === 0) {
+			return !words ? "zero" : words.join(" ").replace(/,$/, "");
 		}
 		// First run
-		if (!w_ords) {
-			w_ords = [];
-		}
+		if (!words) words = [];
+		
 		// If negative, prepend “minus”
-		if (n_umber < 0) {
-			w_ords.push('minus');
-			n_umber = Math.abs(n_umber);
+		if (number < 0) {
+			words.push("minus");
+			number = Math.abs(number);
 		}
-		if (n_umber < 20) {
+		if (number < 20) {
 			remainder = 0;
-			word = LESS_THAN_TWENTY[n_umber];
-		} else if (n_umber < Math.HUNDRED) {
-			remainder = n_umber % Math.TEN;
-			word = TENTHS_LESS_THAN_HUNDRED[Math.floor(n_umber / Math.TEN)];
+			word = LESS_THAN_TWENTY[number];
+		} else if (number < Math.HUNDRED) {
+			remainder = number % Math.TEN;
+			word = TENTHS_LESS_THAN_HUNDRED[Math.floor(number / Math.TEN)];
 			// In case of remainder, we need to handle it here to be able to add the “-”
 			if (remainder) {
-				word += '-' + LESS_THAN_TWENTY[remainder];
+				word += "-" + LESS_THAN_TWENTY[remainder];
 				remainder = 0;
 			}
-		} else if (n_umber < Math.THOUSAND) {
-			remainder = n_umber % Math.HUNDRED;
-			word = generateWords(Math.floor(n_umber / Math.HUNDRED)) + ' hundred';
-		} else if (n_umber < Math.MILLION) {
-			remainder = n_umber % Math.THOUSAND;
-			word = generateWords(Math.floor(n_umber / Math.THOUSAND)) + ' thousand,';
-		} else if (n_umber < Math.BILLION) {
-			remainder = n_umber % Math.MILLION;
-			word = generateWords(Math.floor(n_umber / Math.MILLION)) + ' million,';
-		} else if (n_umber < Math.TRILLION) {
-			remainder = n_umber % Math.BILLION;
-			word = generateWords(Math.floor(n_umber / Math.BILLION)) + ' billion,';
-		} else if (n_umber < Math.QUADRILLION) {
-			remainder = n_umber % Math.TRILLION;
-			word = generateWords(Math.floor(n_umber / Math.TRILLION)) + ' trillion,';
+		} else if (number < Math.THOUSAND) {
+			remainder = number % Math.HUNDRED;
+			word = generateWords(Math.floor(number / Math.HUNDRED)) + " hundred";
+		} else if (number < Math.MILLION) {
+			remainder = number % Math.THOUSAND;
+			word = generateWords(Math.floor(number / Math.THOUSAND)) + " thousand,";
+		} else if (number < Math.BILLION) {
+			remainder = number % Math.MILLION;
+			word = generateWords(Math.floor(number / Math.MILLION)) + " million,";
+		} else if (number < Math.TRILLION) {
+			remainder = number % Math.BILLION;
+			word = generateWords(Math.floor(number / Math.BILLION)) + " billion,";
+		} else if (number < Math.QUADRILLION) {
+			remainder = number % Math.TRILLION;
+			word = generateWords(Math.floor(number / Math.TRILLION)) + " trillion,";
 		} else {
-			remainder = n_umber % Math.QUADRILLION;
+			remainder = number % Math.QUADRILLION;
 			word =
-				generateWords(Math.floor(n_umber / Math.QUADRILLION)) + ' quadrillion,';
+				generateWords(Math.floor(number / Math.QUADRILLION)) + " quadrillion,";
 		}
 
-		w_ords.push(word);
-		return generateWords(remainder, w_ords);
+		words.push(word);
+		return generateWords(remainder, words);
 	};
 
-	words = generateWords(number);
-	return words;
+	return generateWords(number);
 };
 
 export default Number;
